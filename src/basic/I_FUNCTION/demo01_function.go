ebfd797da2736449f88a3a3eb0f7aecbbf9b21c2
Jerry <2396971170@qq.com>
2020-07-06 00:13:32 +0800

    64课学习记录

---
+++ src/basic/I_FUNCTION/demo01_function.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func main() { //程序的入口，是一个特殊的函数
+	/*
+		函数function
+		一.概念：
+			具有特定功能的代码，可以被执行多次
+		二.意义：
+			1.可以避免重复性
+			2.增强程序的扩展性
+		三.使用:
+			step1:函数的定义，也叫声明
+			step2:函数的调用，就是执行函数中执行代码的过程
+		四.语法:
+			1.定义函数的语法
+				func funcName(parametername type,parametername type2)(output1 type1,output2 type2){
+					//这里是处理逻辑的代码
+					//返回多个值
+					return value1,value2
+				}
+				A:func,定义函数的关键字
+				B:funcName,函数的名字
+				C:(),函数的标志
+				D:参数列表:形式参数用于接收外部传入函数中的数据
+				E:返回值列表:函数执行后返回给调用处的结果
+
+			2.调用函数的语法
+				函数名(实际参数)
+			函数的调用处，就是函数调用的位置
+
+	*/
+	//t1 := time.Now()
+	//for i := 0; i < 100; i++ {
+	//	for ii := 0; ii <= 999; ii++ {
+	//		fmt.Println(i, ii, i*ii)
+	//	}
+	//}
+	//elapsed := time.Since(t1)
+	//fmt.Println(elapsed)
+	//1.求1-10的和
+	getsum(100)
+
+}
+
+func getsum(n int) {
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += i
+	}
+	fmt.Printf("1到%d的和是:%d\n", n, sum)
+}
